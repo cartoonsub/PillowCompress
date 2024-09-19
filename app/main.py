03808659f7afbff4a7591926221ac97cbe87c091
Any Task <t.series@yandex.ru>
2024-09-19 23:02:22 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,18 +4,19 @@ import Compressor
 
 def main():
     folder = 'C:/Users/tseri/OneDrive/Изображения/3d'
-    FM = fileManager.FileManager(folder)
-    files = FM.getFiles()
+    fm = fileManager.FileManager(folder)
+    files = fm.get_files()
     if not files:
         print('Error: files is empty')
         return
     
-    newFolder = ''
-    FM.createDirectory(newFolder)
-    CM = Compressor.Compressor(files, 90, newFolder)
-    CM.compressFiles()
+    new_folder = ''
+    fm.create_directory(new_folder)
+    cm = Compressor.Compressor(files, 90, new_folder)
+    cm.compress_files()
     # создать статистику по сжатию
     # к примеру размер папки до и после сжатия
+
 
 if __name__ == "__main__":
     main()
