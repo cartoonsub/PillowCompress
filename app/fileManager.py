f0d36fe9b011b2f12994dbf44536113d838bfee8
Any Task <t.series@yandex.ru>
2024-09-20 00:03:06 +0300

    Enhance FileManager to support skipping specified folders during file retrieval

--- app/fileManager.py
+++ app/fileManager.py
@@ -3,12 +3,14 @@ import re
 import shutil
 import sys
 import pprint as pp
+import re
 
 
 class FileManager:
-    def __init__(self, folder: str, dest: str = ''):
+    def __init__(self, folder: str, dest: str = '', skip_folders: list = []):
         self.folder = folder
         self.dest = dest
+        self.skip_folders = skip_folders
         print('folder:', folder)
         if not self.dest:
             self.dest = self.folder
@@ -22,14 +24,28 @@ class FileManager:
         os.chdir(self.folder)
         current_dir = os.getcwd()
         for root, dirs, files in os.walk(current_dir):
+            if not self.is_allowed_folder(root):
+                print('Skip:', root)
+                continue
+
             if not files:
                 continue
             for file in files:
                 current_file = os.path.join(root, file)
                 files_list.append(current_file)
 
-        pp.pprint(files_list)
         return files_list
+    
+    def is_allowed_folder(self, folder: str) -> bool:
+        result = True
+        folderName = os.path.basename(folder)
+        for skip_folder in self.skip_folders:
+            if re.search(skip_folder, folderName, re.IGNORECASE):
+                print('Skip:', folder)
+                result = False
+                break
+                
+        return result
 
     def create_directory(self, folder: str):
         if not folder:
--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import fileManager
 import Compressor
+import pprint as pp
 
 
 def main():
-    folder = 'C:/Users/tseri/OneDrive/Изображения/3d'
-    fm = fileManager.FileManager(folder)
+    folder = 'C:/Users/tseri/YandexDisk/фото/mi11tt2024/Camera/asia/DONE'
+    skip_folders = [
+        r'done',
+    ]
+    fm = fileManager.FileManager(folder, skip_folders=skip_folders)
     files = fm.get_files()
     if not files:
         print('Error: files is empty')
@@ -14,6 +18,7 @@ def main():
     fm.create_directory(new_folder)
     cm = Compressor.Compressor(files, 90, new_folder)
     cm.compress_files()
+
     # создать статистику по сжатию
     # к примеру размер папки до и после сжатия
 
