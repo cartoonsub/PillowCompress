88c02cbd9fa1840f285820894d36ee5935e00e62
Any Task <t.series@yandex.ru>
2024-09-19 22:58:17 +0300

    Update Compressor.py

--- app/Compressor.py
+++ app/Compressor.py
@@ -2,33 +2,31 @@ from PIL import Image
 import os
 
 
-class Compressor():
-    def __init__(self, files: list, quality: int=100, dest: str=''):
+class Compressor:
+    def __init__(self, files: list, quality: int = 100, dest: str = ''):
         self.files = files
         self.quality = quality
         self.dest = dest
 
-    def compressFiles(self):
+    def compress_files(self):
         for file in self.files:
             try:
-                Img = Image.open(file)
+                img = Image.open(file)
 
-                fileName = os.path.basename(file)
-                fileSize = os.path.getsize(file) / (1024 * 1024)
-                # print('Compressing:', fileName, 'size:', fileSize)
-                
+                file_name = os.path.basename(file)
+                file_size = os.path.getsize(file) / (1024 * 1024)
+
                 if self.dest:
-                    newFile = os.path.join(self.dest, fileName)
+                    new_file = os.path.join(self.dest, file_name)
                 else:
-                    newFile = file
+                    new_file = file
 
-                Img.save(newFile, optimize=True, quality=self.quality)
+                img.save(new_file, optimize=True, quality=self.quality)
 
-                newFileSize = os.path.getsize(newFile) / (1024 * 1024)
-                print('Compressed:', fileName, 'size:', fileSize, '->', newFileSize)
-
+                new_file_size = os.path.getsize(new_file) / (1024 * 1024)
+                print(f'Compressed: {file_name} size: {file_size:.2f} MB -> {new_file_size:.2f} MB')
             except Exception as e:
-                print('Error:', e)
+                print(f'Error: {e}')
                 continue
 
 
