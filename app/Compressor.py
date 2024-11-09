9248dfd3a46d8ee3c0d3ae0355d4a29bf6b34ce5
Any Task <t.series@yandex.ru>
2024-11-09 15:42:53 +0300

    fix: исправить логику изменения размера изображения в Compressor
    - Добавлена проверка на максимальные размеры изображения перед изменением размера.
    - Реализована функция get_new_size для вычисления новых размеров изображения с учетом максимальных размеров.
    - Обновлен метод compress_files для использования функции get_new_size.

--- app/Compressor.py
+++ app/Compressor.py
@@ -3,7 +3,7 @@ import os
 
 
 class Compressor:
-    def __init__(self, files: list, quality: int = 100, dest: str = ''):
+    def __init__(self, files: list, quality: int = 100, dest: str = '', max_sizes: dict = {}) -> None:
         self.files = files
         self.quality = quality
         self.dest = dest
@@ -12,6 +12,15 @@ class Compressor:
         for file in self.files:
             try:
                 img = Image.open(file)
+                
+                if self.max_sizes:
+                    width, height = img.size
+                    if not width or not height:
+                        print(f'Error: size is empty {file}')
+                        continue
+
+                    newWidth, newHeight = self.get_new_size(width, height)
+                    img = img.resize((newWidth, newHeight))
 
                 file_name = os.path.basename(file)
                 file_size = os.path.getsize(file) / (1024 * 1024)
@@ -29,6 +38,37 @@ class Compressor:
                 print(f'Error: {e}')
                 continue
 
+    def get_new_size(self, width: int, height: int) -> tuple:
+        max_width = self.max_sizes.get('width', 1920)
+        max_height = self.max_sizes.get('height', 1080)
+
+        if (
+            (width <= max_width and height <= max_height) or
+            (width <= max_height and height <= max_width)
+        ):
+            return width, height
+
+        new_width = width
+        new_height = height
+
+        reverse = False
+        if width < height:
+            reverse = True
+            width, height = height, width
+
+        aspect_ratio = width / height
+
+        if (width / max_width) > (height / max_height):
+            new_width = max_width
+            new_height = int(max_width / aspect_ratio)
+        else:
+            new_height = max_height
+            new_width = int(max_height * aspect_ratio)
+
+        if reverse:
+            new_width, new_height = new_height, new_width
+
+        return new_width, new_height
 
 if __name__ == '__main__':
     pass
--- app/main.py
+++ app/main.py
@@ -1,23 +1,53 @@
 import fileManager
-import Compressor
+import Compressor as ImageCompressor
 import pprint as pp
 
 
+class Compressor:
+    def __init__(self, params: dict) -> None:
+        #  folder : str, new_folder : str = '', skip_folders : list = [], quality : int = 90
+        if not params:
+            raise ValueError('Error: params is empty')
+        
+        if 'folder' not in params:
+            raise ValueError('Error: folder is required')
+        
+        self.folder = params['folder']
+        self.quality = params.get('quality', 90)
+        self.new_folder = params.get('new_folder', '')
+        self.skip_folders = params.get('skip_folders', [])
+        self.max_sizes = params.get('max_sizes', {})
+
+    def run(self) -> None:
+        fm = fileManager.FileManager(self.folder, skip_folders=self.skip_folders)
+        files = fm.get_files()
+        if not files:
+            print('Error: folder is empty')
+            return
+        
+        if self.new_folder:
+            fm.create_directory(self.new_folder)
+
+        cm = ImageCompressor.Compressor(files, self.quality, self.new_folder)
+        cm.compress_files()
+
+
 def main():
-    folder = 'C:/Users/tseri/YandexDisk/фото/mi11tt2024/Camera/asia/DONE'
-    skip_folders = [
-        r'done',
-    ]
-    fm = fileManager.FileManager(folder, skip_folders=skip_folders)
-    files = fm.get_files()
-    if not files:
-        print('Error: files is empty')
-        return
-    
-    new_folder = ''
-    fm.create_directory(new_folder)
-    cm = Compressor.Compressor(files, 90, new_folder)
-    cm.compress_files()
+    params = {
+        'folder': 'C:/Users/tseri/YandexDisk/фото/mi11tt2024/Camera/asia/DONE/острова PhiPhi/noneed',
+        
+        'skip_folders': [
+            r'done',
+        ],
+        'quality': 90,
+        # 'new_folder': 'C:/Users/tseri/OneDrive/Documents/temp',
+        # 'max_sizes': {
+        #     'width': 1920,
+        #     'height': 1080
+        # }
+    }
+
+    Compressor(params).run()
 
     # создать статистику по сжатию
     # к примеру размер папки до и после сжатия
