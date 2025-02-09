0d063fd1f6520647ce6b2f40e849cf5f5bd38c54
Any Task <t.series@yandex.ru>
2025-02-09 19:20:41 +0300

    feat: refactor folder input setup into a separate method for improved readability and organization

--- app/input_frame.py
+++ app/input_frame.py
@@ -6,27 +6,7 @@ class InputFrame(ttk.Frame):
         super().__init__(master, *args, **kwargs)
         self.pack(anchor=NW, fill=BOTH, padx=10, pady=10, expand=True)
 
-        self.errmsg = StringVar()
-        self.folder_entry_var = StringVar()
-
-        self.name_label = ttk.Label(self, text="Folder:")
-        self.name_label.pack(anchor=NW)
-
-        self.check = (self.register(self.is_valid_folder), "%P")
-
-        self.folder_entry = ttk.Entry(self, validate="all",
-                                      validatecommand=self.check, textvariable=self.folder_entry_var)
-        self.folder_entry.place(anchor=NW, relwidth=0.75)
-        self.folder_entry_var.trace_add("write", lambda name, index,
-                                        mode: self.is_valid_folder(self.folder_entry_var.get()))
-
-        self.error_label = ttk.Label(
-            self, foreground="red", textvariable=self.errmsg)
-        self.error_label.pack(padx=5, pady=5, anchor=NW, fill=X)
-
-        self.folder_button = ttk.Button(self, text="Choose folder", padding=[
-                                        8, 2], command=self.choose_folder)
-        self.folder_button.place(anchor=NW, relx=1-0.25)
+        self.create_folder_input()
 
     def choose_folder(self):
         folder_path = filedialog.askdirectory()
@@ -52,3 +32,26 @@ class InputFrame(ttk.Frame):
             self.error_label.config(foreground="red")
 
         return True
+
+    def create_folder_input(self):
+        self.errmsg = StringVar()
+        self.folder_entry_var = StringVar()
+
+        self.name_label = ttk.Label(self, text="Folder:")
+        self.name_label.pack(anchor=NW)
+
+        self.check = (self.register(self.is_valid_folder), "%P")
+
+        self.folder_entry = ttk.Entry(self, validate="all",
+                                      validatecommand=self.check, textvariable=self.folder_entry_var)
+        self.folder_entry.place(anchor=NW, relwidth=0.75)
+        self.folder_entry_var.trace_add("write", lambda name, index,
+                                        mode: self.is_valid_folder(self.folder_entry_var.get()))
+
+        self.error_label = ttk.Label(
+            self, foreground="red", textvariable=self.errmsg)
+        self.error_label.pack(padx=5, pady=5, anchor=NW, fill=X)
+
+        self.folder_button = ttk.Button(self, text="Choose folder", padding=[
+                                        8, 2], command=self.choose_folder)
+        self.folder_button.place(anchor=NW, relx=1-0.25)
