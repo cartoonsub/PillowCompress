fe671aebd95dcc2442ba731c89b2a493cda34529
Any Task <t.series@yandex.ru>
2025-02-10 23:24:59 +0300

    feat: refactor SettingsFrame to add image settings and clean up folder input methods

--- app/SettingsFrame.py
+++ app/SettingsFrame.py
@@ -12,33 +12,21 @@ class SettingsFrame(ttk.Frame):
         for c in range(6): self.columnconfigure(index=c, weight=1)
         for r in range(6): self.rowconfigure(index=r, weight=1)
 
-        self.create_folder_input()
-        self.create_listbox()
+        self.create_image_settings()
 
-    def choose_folder(self):
-        folder_path = filedialog.askdirectory()
-        if not folder_path:
-            self.errmsg.set("Please enter a folder path")
+    def create_image_settings(self):
+        label = ttk.Label(self, text="Image settings", justify="center", borderwidth=2, relief="ridge", anchor="center")
+        label.grid(row=0, column=0, columnspan=3, sticky="nsew")
+        
+        label_quality = ttk.Label(self, text="Quality:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        label_quality.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
-        if not os.path.exists(folder_path):
-            self.errmsg.set("Folder does not exist: " + folder_path)
+        label_maxwidth = ttk.Label(self, text="Max width:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        label_maxwidth.grid(row=2, column=0, columnspan=2, sticky="nsew")
 
-        self.folder_entry.delete(0, END)
-        self.folder_entry.insert(0, folder_path)
-
-    def is_valid_folder(self, folder_path):
-        if not folder_path:
-            self.errmsg.set("Please enter a folder path")
-            self.error_label.config(foreground="red")
-
-        if os.path.exists(folder_path):
-            self.errmsg.set("Correct folder path")
-            self.error_label.config(foreground="green")
-        else:
-            self.errmsg.set("Folder does not exist: " + folder_path)
-            self.error_label.config(foreground="red")
-
-        return True
+        label_maxheight = ttk.Label(self, text="Max height:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        label_maxheight.grid(row=3, column=0, columnspan=2, sticky="nsew")
+        
 
     def create_folder_input(self):
         self.errmsg = StringVar()
@@ -83,15 +71,3 @@ class SettingsFrame(ttk.Frame):
         skip_delete_button = ttk.Button(self, text="Delete folder", padding=[8, 2], command=self.delete_skip_folder)
         skip_delete_button.grid(row=4, column=5, sticky="we", columnspan=1)
         
-    def delete_skip_folder(self):
-        selection = self.skip_folders_listbox.curselection()
-        if not selection:
-            return
-
-        self.skip_folders_listbox.delete(selection[0])
-
-    def add_skip_folder(self):
-        new_skip_folder = self.skip_folders_entry.get()
-        if not new_skip_folder:
-            return
-        self.skip_folders_listbox.insert(0, new_skip_folder)
