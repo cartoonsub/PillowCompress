526a8f3927a64a21e911efbcd44a40778857a082
Any Task <t.series@yandex.ru>
2025-02-11 00:04:33 +0300

    feat: enhance SettingsFrame with quality, max width, and max height settings

--- app/SettingsFrame.py
+++ app/SettingsFrame.py
@@ -18,56 +18,84 @@ class SettingsFrame(ttk.Frame):
         label = ttk.Label(self, text="Image settings", justify="center", borderwidth=2, relief="ridge", anchor="center")
         label.grid(row=0, column=0, columnspan=3, sticky="nsew")
         
-        label_quality = ttk.Label(self, text="Quality:", borderwidth=2, relief="ridge", anchor="w", justify="left")
-        label_quality.grid(row=1, column=0, columnspan=2, sticky="nsew")
+        self.label_quality = ttk.Label(self, text="Quality:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        self.label_quality.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
-        label_maxwidth = ttk.Label(self, text="Max width:", borderwidth=2, relief="ridge", anchor="w", justify="left")
-        label_maxwidth.grid(row=2, column=0, columnspan=2, sticky="nsew")
+        self.label_maxwidth = ttk.Label(self, text="Max width:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        self.label_maxwidth.grid(row=2, column=0, columnspan=2, sticky="nsew")
 
-        label_maxheight = ttk.Label(self, text="Max height:", borderwidth=2, relief="ridge", anchor="w", justify="left")
-        label_maxheight.grid(row=3, column=0, columnspan=2, sticky="nsew")
+        self.label_maxheight = ttk.Label(self, text="Max height:", borderwidth=2, relief="ridge", anchor="w", justify="left")
+        self.label_maxheight.grid(row=3, column=0, columnspan=2, sticky="nsew")
         
-
-    def create_folder_input(self):
-        self.errmsg = StringVar()
-        self.folder_entry_var = StringVar()
-
-        self.name_label = ttk.Label(self, text="Folder:")
-        self.name_label.grid(row=0, column=0, sticky="nw", pady=0)
-
-        self.error_label = ttk.Label(self, foreground="red", textvariable=self.errmsg)
-        self.error_label.grid(row=0, column=2, sticky='nw', pady=0)
-
-        self.check = (self.register(self.is_valid_folder), "%P")
-
-        self.folder_entry = ttk.Entry(self, validate="all", validatecommand=self.check, textvariable=self.folder_entry_var)
-        self.folder_entry.grid(row=1, column=0, columnspan=4, sticky="ew")
-       
-        self.folder_entry_var.trace_add("write", lambda name, index, mode: self.is_valid_folder(self.folder_entry_var.get()))
-
-        self.folder_button = ttk.Button(self, text="Choose folder", padding=[8, 2], command=self.choose_folder)
-        self.folder_button.grid(row=1, column=4, sticky="ew")
-
-    def create_listbox(self):
-        skip_folders_default = ["Done"] # брать потом из конфига
-        self.skip_folders = StringVar(value=skip_folders_default)
-
-        self.skip_label = ttk.Label(self, text="Skip folders:")
-        self.skip_label.grid(row=2, column=0, sticky="nw", pady=0)
-
-        self.grid_rowconfigure(3, weight=0)
-        self.grid_columnconfigure(3, weight=0)
-        self.grid_columnconfigure(4, weight=0)
-
-        self.skip_folders_listbox = Listbox(self, listvariable=self.skip_folders, height=5, yscrollcommand=True)
-        self.skip_folders_listbox.grid(row=3, column=0, sticky="nwes", columnspan=4, rowspan=2)
-
-        self.skip_folders_entry = ttk.Entry(self)
-        self.skip_folders_entry.grid(row=3, column=4, sticky="nwes", rowspan=1, columnspan=2)
-
-        skip_add_button = ttk.Button(self, text="Add folder", padding=[8, 2], command=self.add_skip_folder)
-        skip_add_button.grid(row=4, column=4, sticky="we", columnspan=1)
-
-        skip_delete_button = ttk.Button(self, text="Delete folder", padding=[8, 2], command=self.delete_skip_folder)
-        skip_delete_button.grid(row=4, column=5, sticky="we", columnspan=1)
+        self.quality = StringVar(value=90) # брать потом из конфига
+        self.check_quality = (self.register(self.change_quality), "%P")
+        self.quality.trace_add("write", lambda name, index, mode: self.change_quality())
         
+        self.quality_spinbox = ttk.Spinbox(self, from_=1, to=100, textvariable=self.quality, command=self.change_quality, validatecommand=self.check_quality)
+        self.quality_spinbox.grid(row=1, column=2, columnspan=1, sticky="nw")
+        self.quality_spinbox.set(90)
+
+        self.maxwidth = StringVar(value=1920) # брать потом из конфига
+        self.check_maxwidth = (self.register(self.change_maxwidth), "%P")
+        self.maxwidth.trace_add("write", lambda name, index, mode: self.change_maxwidth())
+        
+        self.maxwidth_spinbox = ttk.Spinbox(self, from_=1, to=10000, textvariable=self.maxwidth, command=self.change_maxwidth, validatecommand=self.check_maxwidth)
+        self.maxwidth_spinbox.grid(row=2, column=2, columnspan=1, sticky="nw")
+        self.maxwidth_spinbox.set(1920)
+
+        self.maxheight = StringVar(value=1080) # брать потом из конфига
+        self.check_maxheight = (self.register(self.change_maxheight), "%P")
+        self.maxheight.trace_add("write", lambda name, index, mode: self.change_maxheight())
+
+        self.maxheight_spinbox = ttk.Spinbox(self, from_=1, to=10000, textvariable=self.maxheight, command=self.change_maxheight, validatecommand=self.check_maxheight)
+        self.maxheight_spinbox.grid(row=3, column=2, columnspan=1, sticky="nw")
+        self.maxheight_spinbox.set(1080)
+    
+    def change_quality(self):
+        try:
+            value = int(self.quality_spinbox.get())
+        except ValueError:
+            self.quality_spinbox.delete(0, END)
+            self.quality_spinbox.insert(0, 90)
+            return
+
+        if value > 100 or value < 1:
+            self.quality_spinbox.delete(0, END)
+            self.quality_spinbox.insert(0, 90)
+            return
+
+        self.quality.set(value)
+        self.label_quality["text"] = f"Quality: {value}"
+
+    def change_maxwidth(self):
+        try:
+            value = int(self.maxwidth_spinbox.get())
+        except ValueError:
+            self.maxwidth_spinbox.delete(0, END)
+            self.maxwidth_spinbox.insert(0, 1920)
+            return
+
+        if value > 10000 or value < 1:
+            self.maxwidth_spinbox.delete(0, END)
+            self.maxwidth_spinbox.insert(0, 1920)
+            return
+
+        self.maxwidth.set(value)
+        self.label_maxwidth["text"] = f"Max width: {value}"
+
+    def change_maxheight(self):
+        try:
+            value = int(self.maxheight_spinbox.get())
+        except ValueError:
+            self.maxheight_spinbox.delete(0, END)
+            self.maxheight_spinbox.insert(0, 1080)
+            return
+
+        if value > 10000 or value < 1:
+            self.maxheight_spinbox.delete(0, END)
+            self.maxheight_spinbox.insert(0, 1080)
+            return
+
+        self.maxheight.set(value)
+        self.label_maxheight["text"] = f"Max height: {value}"
+
